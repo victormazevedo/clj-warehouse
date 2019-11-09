80096acc701e2e06466306e564fdc9703e244e51
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-11-09 20:34:28 -0300

    Create desconto

---
+++ src/estoque/desconto.clj
@@ -0,0 +1,15 @@
+(ns estoque.desconto)
+
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
+  [valor-bruto]
+  (if (> valor-bruto 100)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto         (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (valor-descontado 1000))
+
+(println (valor-descontado 100))
